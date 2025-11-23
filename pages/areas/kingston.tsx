2f47e3761ab5ec057b6d3504b7a75991df143d28
WEDRAWPLANS <architectabbey@gmail.com>
2025-11-23 20:56:03 +0000

    Update kingston.tsx

--- pages/areas/kingston.tsx
+++ pages/areas/kingston.tsx
@@ -9,15 +9,11 @@ const WHATSAPP_LINK =
   "https://wa.me/442036548508?text=Hello%20WEDRAWPLANS%2C%20I%20would%20like%20a%20quote%20for%20plans%20in%20Kingston%20upon%20Thames";
 
 export default function KingstonUponThamesAreaPage() {
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    const data = new FormData(e.currentTarget);
-    console.log(Object.fromEntries(data.entries()));
-    alert(
-      "Thank you. In the live site this form will email WEDRAWPLANS with your Kingston upon Thames enquiry and trigger a same day call back."
-    );
-    e.currentTarget.reset();
-  }
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  await submitBoroughLead(e, {
+    boroughName: "Kingston upon Thames",
+  });
+}
 
   function scrollToForm() {
     const el = document.getElementById("kingston-quote");
