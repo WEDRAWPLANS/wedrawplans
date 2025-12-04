569e26f73d38c7894f9b2f445ba98bbe54aaf280
WEDRAWPLANS <architectabbey@gmail.com>
2025-12-04 12:46:35 +0000

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,33 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Google Analytics 4 for WEDRAWPLANS */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-QZ5L4HLGTQ"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-QZ5L4HLGTQ');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
