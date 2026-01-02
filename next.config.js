f389fecd7e4fc6aaef7f2c8681363d9f0f3a1b3a
WEDRAWPLANS <architectabbey@gmail.com>
2026-01-02 10:53:19 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,40 @@
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
   async redirects() {
     return [
-      // Redirect any borough slug at root to the real /areas/slug page
-      {
-        source:
-          "/:slug(barking-dagenham|barnet|bexley|brent|bromley|camden|croydon|ealing|enfield|greenwich|hackney|hammersmith-fulham|haringey|harrow|havering|hillingdon|hounslow|islington|kensington-chelsea|kingston|lambeth|lewisham|merton|newham|redbridge|richmond|southwark|sutton|tower-hamlets|waltham-forest|wandsworth|westminster-city|surrey-m25)",
-        destination: "/areas/:slug",
-        permanent: true,
-      },
-
-      // Optional: if anyone hits /areas/havering/ with a trailing slash etc, Next handles it anyway
+      { source: "/barking-and-dagenham", destination: "/areas/barking-and-dagenham", permanent: true },
+      { source: "/barnet", destination: "/areas/barnet", permanent: true },
+      { source: "/bexley", destination: "/areas/bexley", permanent: true },
+      { source: "/brent", destination: "/areas/brent", permanent: true },
+      { source: "/bromley", destination: "/areas/bromley", permanent: true },
+      { source: "/camden", destination: "/areas/camden", permanent: true },
+      { source: "/croydon", destination: "/areas/croydon", permanent: true },
+      { source: "/ealing", destination: "/areas/ealing", permanent: true },
+      { source: "/enfield", destination: "/areas/enfield", permanent: true },
+      { source: "/greenwich", destination: "/areas/greenwich", permanent: true },
+      { source: "/hackney", destination: "/areas/hackney", permanent: true },
+      { source: "/hammersmith-and-fulham", destination: "/areas/hammersmith-and-fulham", permanent: true },
+      { source: "/haringey", destination: "/areas/haringey", permanent: true },
+      { source: "/harrow", destination: "/areas/harrow", permanent: true },
+      { source: "/havering", destination: "/areas/havering", permanent: true },
+      { source: "/hillingdon", destination: "/areas/hillingdon", permanent: true },
+      { source: "/hounslow", destination: "/areas/hounslow", permanent: true },
+      { source: "/islington", destination: "/areas/islington", permanent: true },
+      { source: "/kensington-and-chelsea", destination: "/areas/kensington-and-chelsea", permanent: true },
+      { source: "/kingston-upon-thames", destination: "/areas/kingston-upon-thames", permanent: true },
+      { source: "/lambeth", destination: "/areas/lambeth", permanent: true },
+      { source: "/lewisham", destination: "/areas/lewisham", permanent: true },
+      { source: "/merton", destination: "/areas/merton", permanent: true },
+      { source: "/newham", destination: "/areas/newham", permanent: true },
+      { source: "/redbridge", destination: "/areas/redbridge", permanent: true },
+      { source: "/richmond-upon-thames", destination: "/areas/richmond-upon-thames", permanent: true },
+      { source: "/southwark", destination: "/areas/southwark", permanent: true },
+      { source: "/sutton", destination: "/areas/sutton", permanent: true },
+      { source: "/tower-hamlets", destination: "/areas/tower-hamlets", permanent: true },
+      { source: "/waltham-forest", destination: "/areas/waltham-forest", permanent: true },
+      { source: "/wandsworth", destination: "/areas/wandsworth", permanent: true },
+      { source: "/westminster", destination: "/areas/westminster", permanent: true },
+      { source: "/city-of-london", destination: "/areas/city-of-london", permanent: true },
     ];
   },
 };
