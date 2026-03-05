83e70746032237e95d20997a2509f601e78b8f2c
WEDRAWPLANS <architectabbey@gmail.com>
2026-03-05 20:20:03 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import FloatingLeadWidget from "../components/FloatingLeadWidget";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <FloatingLeadWidget />
+    </>
+  );
 }
