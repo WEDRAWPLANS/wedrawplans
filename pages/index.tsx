e43fc7625dbfbda900b264336f7a79d5f5051112
WEDRAWPLANS <architectabbey@gmail.com>
2026-04-04 11:05:04 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -737,7 +737,7 @@ export default function IndexPage() {
   <div className="mx-auto max-w-6xl px-4 py-3 lg:px-6">
     
     {/* TOP BAR */}
-    <div className="flex items-center justify-between">
+   <div className="flex items-center justify-center relative">
 
       {/* LEFT: LOGO */}
       <Link href="/" className="flex items-center">
