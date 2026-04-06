b47ebe012bf8ce999d039571f4cce17d7d444bc7
WEDRAWPLANS <architectabbey@gmail.com>
2026-04-06 12:13:23 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import FloatingLeadWidget from "../components/FloatingLeadWidget";
+import SmartPlanningAssistant from "../components/SmartPlanningAssistant";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <FloatingLeadWidget />
+      <SmartPlanningAssistant />
     </>
   );
 }
